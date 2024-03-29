d7cdd6b018f7c8cea4343de1df6350b75debbeab
Martin Chiquillo <63934927+gojideth@users.noreply.github.com>
2024-03-28 21:00:19 -0500

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,29 @@
 package gojideth.fp.application
 
-import cats.effect.{IO, IOApp}
-import com.comcast.ip4s.{host, port}
-import gojideth.fp.application.Main.domain.Token
+import cats.effect.{ IO, IOApp }
+import com.comcast.ip4s.{ host, port }
+import gojideth.fp.application.Main.domain.PublicRepos
 import org.http4s.ember.server.EmberServerBuilder
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import org.typelevel.log4cats.syntax.LoggerInterpolator
 import org.http4s.*
+import org.http4s.Header.Raw
+import org.http4s.client.{ Client, UnexpectedStatus }
 import org.http4s.dsl.io.*
-import play.api.libs.json.{JsError, JsSuccess, Json, Reads, Writes, __}
-import pureconfig.{ConfigReader, ConfigSource}
+import org.http4s.ember.client.EmberClientBuilder
+import org.typelevel.ci.CIString
+import pureconfig.*
 import pureconfig.generic.derivation.default.*
+import cats.syntax.all.*
+import play.api.libs.json.*
+import play.api.libs.*
 object Main extends IOApp.Simple {
+  import syntax._
+
+  final case class Token(token: String) derives ConfigReader {
+    override def toString: String = token
+  }
 
   given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
@@ -20,18 +31,65 @@ object Main extends IOApp.Simple {
     (for {
       _ <- info"starting server".toResource
       token <- IO.delay(ConfigSource.default.loadOrThrow[Token]).toResource
-      _ <- EmberServerBuilder.default[IO].withHost(host"localhost").withPort(port"9000").withHttpApp(routes.orNotFound).build
+      client <- EmberClientBuilder.default[IO].build
+      _ <- EmberServerBuilder
+             .default[IO]
+             .withHost(host"localhost")
+             .withPort(port"9001")
+             .withHttpApp(routes(client, token).orNotFound)
+             .build
     } yield ()).useForever
 
-  def routes: HttpRoutes[IO] =
-    HttpRoutes.of[IO] { case GET -> Root =>
-      Ok("Hi I don't understand anything xD")
-    case GET -> Root / "org" / orgName => Ok(orgName)
+  def publicRepos(orgName: String) = s"https://api.github.com/orgs/$orgName"
+  def contributorsUrl(repoName: String, orgName: String, page: Int): String =
+    s"https://api.github.com/repos/$orgName/$repoName/contributors?per_page=100&page=$page"
+  def repos(orgName: String, page: Int): String =
+    s"https://api.github.com/orgs/$orgName/repos?per_page=100&page=$page"
+
+  def req(uri: Uri)(using token: Token) = Request[IO](Method.GET, uri)
+    .putHeaders(Raw(CIString("Authorization"), s"Bearer ${token.token}"))
+
+  def uri(url: String): IO[Uri] = IO.fromEither(Uri.fromString(url))
+
+  def fetch[A: Reads](uri: Uri, client: Client[IO], default: => A)(using token: Token): IO[A] =
+    client
+      .expect[String](req(uri))
+      .map(_.into[A])
+      .onError {
+        case org.http4s.client.UnexpectedStatus(Status.Unauthorized, _, _) =>
+          error"GitHub token is either expired or absent, please check `token` key in src/main/resources/application.conf"
+        case other => error"other"
+      }
+      .handleErrorWith(_ => warn"returning default value: $default for $uri due to unexpected error" as default)
+
+  def routes(client: Client[IO], token: Token): HttpRoutes[IO] = {
+    given tk: Token = token
+    HttpRoutes.of[IO] {
+      case GET -> Root =>
+        Ok(s"Hi I don't understand anything xD $tk")
+      case GET -> Root / "org" / orgName =>
+        for {
+          publicReposURI <- uri(publicRepos(orgName))
+          _ <- info"accepted $orgName"
+          publicRepos <- fetch[PublicRepos](publicReposURI, client, PublicRepos.Empty)
+          _ <- info"fetching the amount of available repositories for $orgName"
+          pages = (1 to (publicRepos.value / 100) + 1).toVector
+          response <- Ok(s"# of public repos: ${publicRepos}, # of parallel requests: ${pages.size}")
+        } yield response
     }
+  }
+
+  object syntax {
+    extension (self: String) def into[A](using r: Reads[A]): A = Json.parse(self).as[A]
+    extension [A](self: A) def toJson(using w: Writes[A]): String = Json.prettyPrint(w.writes(self))
+  }
 
   object domain {
 
-    import cats.syntax.all.*
+    import play.api.libs.json.*
+    import play.api.libs.*
+    import Reads.{ IntReads, StringReads }
+
     opaque type PublicRepos = Int
 
     object PublicRepos {
@@ -42,8 +100,7 @@ object Main extends IOApp.Simple {
 
     extension (repos: PublicRepos) def value: Int = repos
 
-    given ReadsPublicRepos: Reads[PublicRepos] =
-      (__ \ "public_repos").read[Int].map(PublicRepos.apply)
+    given ReadsPublicRepos: Reads[PublicRepos] = (__ \ "public_repos").read[Int].map(PublicRepos.apply)
 
     opaque type RepoName = String
     object RepoName {
@@ -68,10 +125,6 @@ object Main extends IOApp.Simple {
 
     final case class Contributions(count: Long, contributors: Seq[Contributor])
     given WritesContributions: Writes[Contributions] = Json.writes[Contributions]
-    
-    final case class Token(token: String) derives ConfigReader{
-      override def toString: String = token
-    }
 
   }
 
